4719426f1da67444539c649892af599751aa0599
asifalam515 <asibulalam515@gmail.com>
2025-11-02 10:57:46 +0600

    sorted by high rating

---
+++ data_transformation/data_transformation.js
@@ -0,0 +1,81 @@
+const rowData = [
+  {
+    id: 1,
+    name: "Wireless Bluetooth Headphones",
+    price: 59.99,
+    quantity: 2,
+    rating: 5,
+    image: "/images/headphones.jpg",
+    category: "Electronics",
+    inStock: true,
+  },
+  {
+    id: 2,
+    name: "Smart Fitness Watch",
+    price: 89.5,
+    quantity: 1,
+    rating: 2,
+    image: "/images/fitness-watch.jpg",
+    category: "Wearables",
+    inStock: true,
+  },
+  {
+    id: 3,
+    name: "Ergonomic Office Chair",
+    price: 199.99,
+    quantity: 1,
+    rating: 3,
+    image: "/images/office-chair.jpg",
+    category: "Furniture",
+    inStock: true,
+  },
+  {
+    id: 4,
+    name: "Mechanical Gaming Keyboard",
+    price: 79.99,
+    quantity: 1,
+    rating: 2,
+    image: "/images/gaming-keyboard.jpg",
+    category: "Electronics",
+    inStock: false,
+  },
+  {
+    id: 5,
+    name: "Stainless Steel Water Bottle",
+    price: 24.99,
+    quantity: 3,
+    rating: 3,
+    image: "/images/water-bottle.jpg",
+    category: "Accessories",
+    inStock: true,
+  },
+  {
+    id: 6,
+    name: "Portable Power Bank 20000mAh",
+    price: 49.99,
+    quantity: 1,
+    rating: 3,
+    image: "/images/power-bank.jpg",
+    category: "Electronics",
+    inStock: true,
+  },
+
+  {
+    id: 7,
+    name: "Noise Cancelling Earbuds",
+    price: 69.99,
+    quantity: 1,
+    rating: 4,
+    image: "/images/earbuds.jpg",
+    category: "Electronics",
+    inStock: false,
+  },
+];
+//filter out "Electronics" product only
+const electronicsGoods = rowData.filter((product) => {
+  return product.category == "Electronics";
+});
+// now get only best rating product
+const sortedLowToHighRating = electronicsGoods.sort(
+  (a, b) => a.rating - b.rating
+);
